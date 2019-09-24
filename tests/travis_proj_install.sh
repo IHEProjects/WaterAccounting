fedd621a897f84e47247640e36c8a19db5a6ca58
quanpan <quanpan302@hotmail.com>
2019-09-24 23:04:37 +0200

    Update travis_proj_install.sh
    Based on rasterio

--- tests/travis_proj_install.sh
+++ tests/travis_proj_install.sh
@@ -10,6 +10,38 @@
 
 set -e
 
+########################################
+# GDAL
+########################################
+# Create build dir if not exists
+if [ ! -d "$PROJBUILD" ]; then
+  mkdir $PROJBUILD;
+fi
+
+if [ ! -d "$PROJINST" ]; then
+  mkdir $PROJINST;
+fi
+
+ls -l $PROJINST
+
+echo "PROJ VERSION: $PROJVERSION"
+
+if [ ! -d "$PROJINST/gdal-$GDALVERSION/share/proj" ]; then
+    cd $PROJBUILD
+    wget -q https://download.osgeo.org/proj/proj-$PROJVERSION.tar.gz
+    tar -xzf proj-$PROJVERSION.tar.gz
+    cd proj-$PROJVERSION
+    ./configure --prefix=$PROJINST/gdal-$GDALVERSION
+    make -s -j 2
+    make install
+fi
+
+# change back to travis build dir
+cd $TRAVIS_BUILD_DIR
+
+########################################
+# Conda
+########################################
 if [[ "$DISTRIB" == "conda" ]]; then
     # Deactivate the travis-provided virtual environment and setup a
     # conda-based environment instead
@@ -40,7 +72,9 @@ if [[ "$DISTRIB" == "conda" ]]; then
     source activate ./.venv
 fi
 
-# for all
+########################################
+# Unit Test
+########################################
 pip install -U pip setuptools
 pip install tox
 
